d5f8d49ef9d9a030e0bf8bb336b393213b9c4792
Sebastian Kylander <developer@gaztin.com>
2020-01-16 19:29:04 +0100

    Add text field and button for running premake

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -1,8 +1,52 @@
 #include "MainWindow.h"
 
+extern "C"
+{
+#include "premake.h"
+}
+
 MainWindow::MainWindow( QWidget* parent )
 	: QMainWindow( parent )
 {
 	m_ui.setupUi( this );
+
+	L = luaL_newstate();
+	luaL_openlibs( L );
+
+	if( premake_init( L ) == OKAY )
+	{
+		connect( m_ui.pushButtonRun, &QPushButton::clicked, this, &MainWindow::RunScript );
+	}
+	else
+	{
+		m_ui.pushButtonRun->setDisabled( true );
+	}
 }
 
+MainWindow::~MainWindow( void )
+{
+	lua_close( L );
+}
+
+void MainWindow::RunScript( void )
+{
+	QString script_arg = "--script=";
+	script_arg += m_ui.lineEditScriptPath->text();
+
+	const int   argc = 3;
+	const char* argv[ argc ]
+	{
+		qPrintable( qApp->applicationFilePath() ),
+		"vs2017",
+		qPrintable( script_arg ),
+	};
+
+	if( premake_execute( L, argc, argv, "src/_premake_main.lua" ) == OKAY )
+	{
+		m_ui.statusbar->showMessage( "Execute succeeded!" );
+	}
+	else
+	{
+		m_ui.statusbar->showMessage( "Execute failed.." );
+	}
+}
--- gui/MainWindow.h
+++ gui/MainWindow.h
@@ -1,16 +1,25 @@
 #pragma once
 #include "ui_MainWindow.h"
 
+struct lua_State;
+
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
 public:
 
-	MainWindow( QWidget* parent = nullptr );
+	 MainWindow( QWidget* parent = nullptr );
+	~MainWindow( void );
+
+private slots:
+
+	void RunScript( void );
 
 private:
 
 	Ui::MainWindow m_ui;
+
+	lua_State* L;
 
 };
